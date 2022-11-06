ce419ecfd2e30c8cf445312937e6c5ed859f3d5a
SBK <48666130+bernie6401@users.noreply.github.com>
2022-11-06 22:33:14 +0800

    revise test case

--- Project2/nachos-4.0/code/test/sleep2.c
+++ Project2/nachos-4.0/code/test/sleep2.c
@@ -1,8 +1,8 @@
 #include "syscall.h"
 main() {
     int i;
-    for(i = 0; i < 20; i++) {
-        Sleep(100000);
+    for(i = 0; i < 5; i++) {
+        Sleep(10000000);
         PrintInt(10);
     }
     return 0;
